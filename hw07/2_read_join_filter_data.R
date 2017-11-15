3442a697499bd88bf168333a61db529f919b5384
Santiago David <sdrrock@gmail.com>
2017-11-15 10:25:13 -0800

    starting 3_analysis

--- hw07/2_read_join_filter_data.R
+++ hw07/2_read_join_filter_data.R
@@ -14,17 +14,17 @@ glimpse(bird_func_data)
 glimpse(bird_morph_data)
 
 # Fix scientific names in bird_morph_data to be one binomial name, so that we can join based on 
-# that variable
+# that variable --> Scientific
 bird_morph_data <- bird_morph_data %>% 
   mutate(Scientific = str_c(Genus, Species, sep = " "))
 
 # There are 9995 species in func_data vs 2366 species in morph_data
 # Join both databases, but only keeping the species in common
 # select only meaningful variables for functional or morphological traits
-
 bird_data_clean <- bird_morph_data %>% 
   inner_join(bird_func_data) %>% 
   select(Family, Scientific, `BodyMass-Value`, Length:`Bill D`, `Diet-Inv`:`Diet-5Cat`, 
          `ForStrat-watbelowsurf`:PelagicSpecialist, Nocturnal)
 
+# export data to file
 write_csv(bird_data_clean, "bird_data_clean.csv")
---
+++ hw07/3_analysis.R
@@ -0,0 +1,7 @@
+# Run analysis and produce graphs
+
+library(tidyverse)
+
+bird_data <- read_csv("bird_data_clean.csv")
+
+glimpse(bird_data)
