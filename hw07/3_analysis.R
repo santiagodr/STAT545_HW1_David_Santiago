822f911993c075262d43875b09b05c2357c02c0d
Santiago David <sdrrock@gmail.com>
2017-11-15 17:48:42 -0800

    fig 3 ready

--- hw07/3_analysis.R
+++ hw07/3_analysis.R
@@ -21,30 +21,46 @@ ggsave("fig1_spbydiet.png", plot = fig1, width = 6, height = 4)
 # subsetting data for 9 species and rearranging data to create two variables and
 # percentange of use of each diet type per species
 
-bird_diet <- bird_data %>%
+bird_sample <- bird_data %>%
   sample_n(9, replace = TRUE) %>% 
   select(Scientific, `Diet-Inv`:`Diet-PlantO`) %>% 
   gather(diet_types, percentage_use, `Diet-Inv`:`Diet-PlantO`)
 
-fig2 <- bird_diet %>% 
+fig2 <- bird_sample %>% 
   ggplot(aes(y = percentage_use, x = diet_types)) + theme_bw() +
   geom_bar(position = "dodge", stat = "identity", fill = "#A2CD5A") +
   theme(axis.text.x = element_text(angle=90, hjust = 1)) +
   labs(x = "Diet categories", y = "Estimated percentage use (%)") +
   facet_wrap(~Scientific) 
-ggsave("fig2_sample_.png", plot = fig1, width = 6, height = 4)
+ggsave("fig2_sample_diet_use.png", plot = fig2, width = 6, height = 4)
+
+#### Now into more complex stuff
+# Lets show the distribution of traits per diet category
+bird_diet <- bird_data %>%
+  select(Scientific, `Diet-5Cat`, `BodyMass-Value`:`Bill D`) %>% 
+  gather(trait, value, `BodyMass-Value`:`Bill D`)
+
+bird_diet <- bird_diet %>% 
+  mutate(`Diet-5Cat` = as.factor(`Diet-5Cat`),
+         trait = as.factor(trait),
+         value = as.double(value))
+
+# Plot to show distribution of each morphological trait by Diet category
+fig3 <- bird_diet %>% 
+ggplot(aes(x = `Diet-5Cat`, y = value, fill = `Diet-5Cat`)) +
+  geom_boxplot() + theme_bw() +
+  theme(axis.text.x = element_text(angle=90, hjust = 1)) +
+  labs(x = "Diet categories", y = "Mean value of trait") +
+  facet_wrap(~trait, scales = "free_y")
+ggsave("fig3_trait_distribution_per_diet.png", plot = fig3, width = 6, height = 6)
 
 
 
+#one trait
+bird_data %>% 
+  ggplot(aes(x = `Diet-5Cat`, y = `BodyMass-Value`)) +
+  geom_boxplot() +
+  labs(x = "Diet categories", y = "Mean value of trait") +
+  theme_bw()
 
-
-ggplot(bird_diet, aes(percentage_use)) + 
-  scale_fill_brewer(palette = "Spectral") +
-  geom_histogram(aes(fill = diet_types),
-                 binwidth = 5,
-                 col = "black",
-                 size = .5) +
-  labs(x = "Percentage of use", y = "Number of species") +
-  theme_classic()
- 
 
